f2347f93d4456d847c65e56e933134b225e0d1eb
rajan <rajan@IDIOTBOX>
2017-07-26 00:45:15 -0400

    Driver Class

--- AbstarctPlanets_300926123/Program.cs
+++ AbstarctPlanets_300926123/Program.cs
@@ -11,7 +11,7 @@ using System.Threading.Tasks;
 /// 7/25/2017
 /// Comp123 Assignment04 Abstract Classes
 /// This is a Abstarct Planet Class
-/// Version 0.1 Creaed Abstract Planet Class
+/// This is the driver class
 /// </summary>
 
 namespace AbstarctPlanets_300926123
@@ -20,6 +20,20 @@ namespace AbstarctPlanets_300926123
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("================================================================================");
+            GiantPlanet giantPlanet = new GiantPlanet("Jupiter", 139822, 1898000, "Hydrogen", 8, 5);
+            Console.WriteLine(giantPlanet.ToString());
+            Console.WriteLine("================================================================================");
+            Console.WriteLine("================================================================================");
+            TerrestialPlanet terrestialPlanet = new TerrestialPlanet("Earth", 12742, 5972000, true, 1);
+            Console.WriteLine(terrestialPlanet.ToString());
+            Console.WriteLine("================================================================================");
+            WaitForAnyKey();
+        }
+        public static void WaitForAnyKey()
+        {
+            Console.ReadKey();
         }
     }
-}
+    }
+
