07fcefe41e290671c3af4aa28980872477592fa0
rajan <rajan@IDIOTBOX>
2017-07-25 23:49:24 -0400

    Created Giant Planet

---
+++ AbstarctPlanets_300926123/GiantPlanet.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Rajandeep Kaur Dhaliwal
+/// 300926123
+/// 7/25/2017
+/// Comp123 Assignment04 Abstract Classes
+/// This is a Giant Planet Class
+/// Version 0.1 Creaed Giant Planet Class
+/// </summary>
+/// 
+
+namespace AbstarctPlanets_300926123
+{
+    class GiantPlanet
+    {
+        //PRIVATE INSTANCE VARIABLES
+
+
+        //CONSTRUCTOR----------------------------------------------------
+    }
+}
