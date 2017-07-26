2ab5f69f1b4d3184e7459fbb6743f249f3299f43
rajan <rajan@IDIOTBOX>
2017-07-26 00:35:35 -0400

    Created Interface IHAbitable

---
+++ AbstarctPlanets_300926123/IHabitable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Rajandeep Kaur Dhaliwal
+/// 300926123
+/// 7/26/2017
+/// Comp123 Assignment04 Abstract Classes
+/// This is IHabiyable Interface
+/// Version 0.1 Created TerrestialPlanet Class
+/// /// </summary>
+namespace AbstarctPlanets_300926123
+{
+    interface IHabitable
+    {
+        bool IHabitable();
+    }
+}
