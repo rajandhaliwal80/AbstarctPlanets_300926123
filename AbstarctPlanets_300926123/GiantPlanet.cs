cbeeda81d7a7da76c56af26b6b6c0782be813c52
rajan <rajan@IDIOTBOX>
2017-07-26 00:13:59 -0400

    Added Constructor and derived interfaces method

--- AbstarctPlanets_300926123/GiantPlanet.cs
+++ AbstarctPlanets_300926123/GiantPlanet.cs
@@ -10,13 +10,13 @@ using System.Threading.Tasks;
 /// 7/25/2017
 /// Comp123 Assignment04 Abstract Classes
 /// This is a Giant Planet Class
-/// Version 0.2 Added Private Instace Variables
-/// </summary>
+/// Version 0.3 Added Constructor
+/// /// </summary>
 /// 
 
 namespace AbstarctPlanets_300926123
 {
-    class GiantPlanet
+    class GiantPlanet : Planet
     {
         //PRIVATE INSTANCE VARIABLES
         private string _type;
@@ -24,5 +24,43 @@ namespace AbstarctPlanets_300926123
         private int _ringCount;
 
         //CONSTRUCTOR----------------------------------------------------
+        /// <summary>
+        /// this is a giantplanet constructor its takes 6 arguments. 3 of them are derived from planet class.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="diameter"></param>
+        /// <param name="mass"></param>
+        /// <param name="type"></param>
+        /// <param name="moonCount"></param>
+        /// <param name="ringCount"></param>
+        public GiantPlanet(string name, double diameter, double mass, string type, int moonCount, int ringCount) : base(name, diameter, mass)
+        {
+            _type = type;
+            _moonCount = moonCount;
+            _ringCount = ringCount;
+        }
+        //defining derived Interface Methods
+        public bool HasMoons()
+        {
+            if (MoonCount > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool HasRings()
+        {
+            if (RingCount > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
