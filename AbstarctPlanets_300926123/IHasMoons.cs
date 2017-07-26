73332940612a8bfd2c5731eda68ea46d4d6b7b34
rajan <rajan@IDIOTBOX>
2017-07-26 00:30:49 -0400

    Created Interface IHasMoons

---
+++ AbstarctPlanets_300926123/IHasMoons.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Rajandeep Kaur Dhaliwal
+/// 300926123
+/// 7/26/2017
+/// Comp123 Assignment04 Abstract Classes
+/// This is IHasMoons Interface
+/// Version 0.1 Created TerrestialPlanet Class
+/// /// </summary>
+
+namespace AbstarctPlanets_300926123
+{
+   public  interface IHasMoons
+    {
+        bool Habitable();
+    }
+}
