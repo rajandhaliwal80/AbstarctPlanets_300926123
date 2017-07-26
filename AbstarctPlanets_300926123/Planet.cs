7970e7b1b4159dec39639153d929425fd433a8f4
rajan <rajan@IDIOTBOX>
2017-07-25 23:43:58 -0400

    Added private instance variables

--- AbstarctPlanets_300926123/Planet.cs
+++ AbstarctPlanets_300926123/Planet.cs
@@ -19,10 +19,17 @@ namespace AbstarctPlanets_300926123
     class Planet
     {
         //PRIVATE INSTANCE VARIABLES 
+        private double _diameter;
+        private double _mass;
+        private int _moonCount;
+        private string _name;
+        private double _orbitalPeriod;
+        private int _ringCount;
+        private double _rotationPeriod;
 
         //PUBLIC PROPERTIES
 
         //CONSTRUCTORS-----------------------------------------------
-        
+
     }
 }
