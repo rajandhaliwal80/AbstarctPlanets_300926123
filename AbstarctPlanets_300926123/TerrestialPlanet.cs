56a6cd38a2cd4b14a25b675b7a4262a198a9fd76
rajan <rajan@IDIOTBOX>
2017-07-26 00:21:35 -0400

    Created Terrestial Planet Class

---
+++ AbstarctPlanets_300926123/TerrestialPlanet.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Rajandeep Kaur Dhaliwal
+/// 300926123
+/// 7/25/2017
+/// Comp123 Assignment04 Abstract Classes
+/// This is a Giant Planet Class
+/// Version 0.1 Created TerrestialPlanet Class
+/// /// </summary>
+
+namespace AbstarctPlanets_300926123
+{
+    class TerrestialPlanet
+    {
+        //PRIVATE INSTANCE VARIABLES
+
+        //Constructor---------------------------------------------
+
+        //Implemented Interface Method
+    }
+}
