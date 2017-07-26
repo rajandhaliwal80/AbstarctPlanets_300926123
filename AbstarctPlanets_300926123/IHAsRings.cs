70d632149acc3ff95b6b22e009e5c897030e35c8
rajan <rajan@IDIOTBOX>
2017-07-26 00:34:01 -0400

    Created Interface IHasRings

--- AbstarctPlanets_300926123/IHAsRings.cs
+++ AbstarctPlanets_300926123/IHAsRings.cs
@@ -4,9 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+/// <summary>
+/// Rajandeep Kaur Dhaliwal
+/// 300926123
+/// 7/26/2017
+/// Comp123 Assignment04 Abstract Classes
+/// This is IHasRings Interface
+/// Version 0.1 Created TerrestialPlanet Class
+/// /// </summary>
 namespace AbstarctPlanets_300926123
 {
-    interface IHAsRings
+    public interface IHAsRings
     {
+        bool HasRings();
     }
 }
